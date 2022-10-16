c4b53e2ee6938a86fc83caee8ae720a90037e8e9
Furball <33634370+Eeveelution@users.noreply.github.com>
2022-10-16 20:41:04 +0200

    made server continuously listen instead of stopping after 1 file

--- UPLOADER_SERVER/main.go
+++ UPLOADER_SERVER/main.go
@@ -11,79 +11,85 @@ import (
 
 func main() {
 	if len(os.Args) != 2 {
-		fmt.Printf("You need to supply a server location!")
+		fmt.Printf("You need to supply a server location!\n")
 		return
 	}
 
 	listen, err := net.Listen("tcp", os.Args[1])
 
 	if err != nil {
-		fmt.Printf("Failed to set up TCP Server!")
+		fmt.Printf("Failed to set up TCP Server!\n")
 		return
 	}
 
-	conn, err := listen.Accept()
-
-	time.Sleep(1000 * time.Millisecond)
-
-	if err != nil {
-		fmt.Printf("Failed to accept client!")
-		listen.Close()
-		return
-	}
-
-	header := make([]byte, 8)
-
-	read, err := conn.Read(header)
-
-	if err != nil {
-		fmt.Printf("Failed to read!")
-		listen.Close()
-		return
-	}
-
-	actualData := header[:read]
-
-	reader := bytes.NewBuffer(actualData)
-
-	var fileSize int32
-	var filenameSize int32
-
-	binary.Read(reader, binary.LittleEndian, &fileSize)
-	binary.Read(reader, binary.LittleEndian, &filenameSize)
-
-	filenameBytes := make([]byte, filenameSize)
-
-	conn.Read(filenameBytes)
-
-	filenameString := string(filenameBytes)
-
-	time.Sleep(1000 * time.Millisecond)
-
-	fileData := []byte{}
+	fmt.Printf("Server started!\n")
 
 	for {
-		time.Sleep(1000 * time.Millisecond)
 
-		readData := make([]byte, fileSize)
-		fileRead, _ := conn.Read(readData)
+		conn, err := listen.Accept()
 
-		fmt.Printf("Read %d bytes\n", fileRead)
+		go func(conn net.Conn) {
+			time.Sleep(1000 * time.Millisecond)
 
-		if fileRead == 0 {
-			break
-		}
+			if err != nil {
+				fmt.Printf("Failed to accept client!\n")
+				listen.Close()
+				return
+			}
 
-		for i := 0; i != fileRead; i++ {
-			fileData = append(fileData, readData[i])
-		}
+			header := make([]byte, 8)
+
+			read, err := conn.Read(header)
+
+			if err != nil {
+				fmt.Printf("Failed to read!\n")
+				listen.Close()
+				return
+			}
+
+			actualData := header[:read]
+
+			reader := bytes.NewBuffer(actualData)
+
+			var fileSize int32
+			var filenameSize int32
+
+			binary.Read(reader, binary.LittleEndian, &fileSize)
+			binary.Read(reader, binary.LittleEndian, &filenameSize)
+
+			filenameBytes := make([]byte, filenameSize)
+
+			conn.Read(filenameBytes)
+
+			filenameString := string(filenameBytes)
+
+			time.Sleep(1000 * time.Millisecond)
+
+			fileData := []byte{}
+
+			for {
+				time.Sleep(250 * time.Millisecond)
+
+				readData := make([]byte, fileSize)
+				fileRead, _ := conn.Read(readData)
+
+				if fileRead == 0 {
+					break
+				}
+
+				for i := 0; i != fileRead; i++ {
+					fileData = append(fileData, readData[i])
+				}
+			}
+
+			file, _ := os.Create(filenameString)
+
+			binary.Write(file, binary.LittleEndian, &fileData)
+
+			file.Close()
+
+			fmt.Printf("Successfully received %s! Size: %d\n", filenameString, fileSize)
+		}(conn)
 	}
 
-	file, _ := os.Create(filenameString)
-
-	binary.Write(file, binary.LittleEndian, &fileData)
-
-	file.Close()
-
-	fmt.Printf("Successfully received %s! Size: %d", filenameString, fileSize)
 }
